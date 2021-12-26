b74ccbc95c397859b60a4b4af6d8e8feaf443838
acebk <81196056+acebk@users.noreply.github.com>
2021-12-26 11:31:27 -0500

    New files and updates
    Added index page and initialized flask code

--- database.py
+++ database.py
@@ -4,8 +4,5 @@ def get_all_facts():
     conn=sqlite3.connect('factbook.db')
     cur = conn.cursor()
     cur.execute("""SELECT * FROM facts""")
-
     rows = cur.fetchall()
-
-    for row in rows:
-        print(row[0])
+    return rows
